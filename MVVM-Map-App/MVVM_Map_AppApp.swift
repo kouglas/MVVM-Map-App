a7558815790adf2f07f2b55b935644f748b5318d
Kari <kouglas2@gmail.com>
2024-06-29 18:09:04 -0500

    Initial Commit

---
+++ MVVM-Map-App/MVVM_Map_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_Map_AppApp.swift
+//  MVVM-Map-App
+//
+//  Created by Kari Douglas on 6/29/24.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_Map_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

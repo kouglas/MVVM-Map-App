5e111a0c4b5c4c62e7b7cfcd4b20701eff1d454b
Kari <kouglas2@gmail.com>
2024-07-02 16:46:15 -0500

    change from vstack to hstack

--- MVVM-Map-App/ContentView.swift
+++ MVVM-Map-App/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        HStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
